1211f3a29a706c5f1ef0a1dc72042c5fc5bbae78
shivank goyal <goyal.shivank790.com>
2021-02-21 18:16:32 +0530

    first commit

---
+++ development/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Netflix from './Apps/netflix'
+
+const Reactroot = document.getElementById("root")
+ReactDOM.render(<Netflix />,Reactroot)     
