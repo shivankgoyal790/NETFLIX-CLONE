a72a4401a610737bacc2654c5458644ed5215f15
shivank goyal <goyal.shivank790.com>
2021-02-27 23:31:11 +0530

    loginpage1

--- development/src/Components/Login/login.js
+++ development/src/Components/Login/login.js
@@ -3,9 +3,9 @@ import "./login.css"
 import logo from 'G:/react_practice/development/src/Components/images/netflix1.jpg'
 import logo1 from 'G:/react_practice/development/src/Components/images/netflix2.png'
 /*import logo from 'G:/react_practice/development/src/Components/images/t1.jpg'*/
-/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF} from '@fortawesome/free-brands-svg-icons'
-import { faGoogle} from '@fortawesome/free-brands-svg-icons'
+/*import { faGoogle} from '@fortawesome/free-brands-svg-icons'
 import { faGithub} from '@fortawesome/free-brands-svg-icons'
 import { faTelegram} from '@fortawesome/free-brands-svg-icons'*/
 
@@ -29,14 +29,6 @@ const myfunction= ()=>
 }
 
 
-const myfunction2 = () =>{
-
-    var x= document.getElementsByClassName("inpt");
-    for(var i=0;i<2;i++){
-        x[i].value = "";
-    }
-}
-
 
 
 
@@ -53,20 +45,27 @@ return(
         
             <div className="inside">
             <h2 className="line4">Sign In</h2>
-            <p>Enter Name
-            </p>
-            <input type="text" placeholder="Enter your name" className="inpt5" />
-            <p className="alert1">*Field empty</p>
-            <p>Enter Password</p>
-            <input type="password" placeholder="Enter your password" className="inpt5" /><br />
+            <input type="text" placeholder="Email or phone number " className="inpt5" /><br/><br/>
+            <p className="alert1">Please enter a valid email or phone number</p>
+            <input type="password" placeholder="Password" className="inpt5" />
             <p className="alert1">*Field empty</p>
             <p className="alert1">*Minimum 8 characters</p>
-            <input type="checkbox" />Remember Me
             <br />
             <br />
-            <button className="btn" onClick={myfunction}>SUBMIT</button>&emsp;
-            <button className="btn" onClick={myfunction2}>RESET</button>
-            <br /><br />
+            <br/>
+            <button className="btn6" onClick={myfunction}>Sign In</button>&emsp;
+            <br /><br/>
+            <div className="line6">
+            <input type="checkbox"  /><span className="remember">Remember Me</span>
+            <span className="help">Need help?</span>
+            </div>
+            <br/><br/>
+            <button className="facebookbtn"><FontAwesomeIcon icon={faFacebookF} className="icon"/> Login with facebook</button>
+            <br /><br/>
+            <span className="new">
+                New to netflix
+            </span>
+            <span className="sign">Sign up now</span> 
             </div>
            
            
