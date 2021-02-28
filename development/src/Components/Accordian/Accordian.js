381e60f999503dfbe7b620e97b3530ec9532ed21
shivank goyal <goyal.shivank790@gmail.com>
2021-02-28 22:00:20 +0530

    Accordianfix

--- development/src/Components/Accordian/Accordian.js
+++ development/src/Components/Accordian/Accordian.js
@@ -43,7 +43,28 @@ return(
             aksndljbasdasdsa jfbasd
         </div>
         <div  className="divstyle" onClick={myfunction}>Where can i watch?</div>
-        <div className="hi">jab skjdbakjsfbjak sbdklas</div>
+        <div className="hi"><p>Watch anywhere, anytime, on an unlimited number of devices. 
+        Sign in with your Netflix account to watch instantly on the web at 
+        netflix.com from your personal computer or on any internet-
+        connected device that offers the Netflix app, including smart TVs, 
+        smartphones, tablets, streaming media players and game consoles.</p>
+        <p>
+        You can also download your favourite shows with the iOS,
+         Android, or Windows 10 app. Use downloads to watch while
+          you're on the go and without an internet connection. Take Netflix 
+          with you anywhere.
+          </p>
+          </div>
+
+        <div  className="divstyle" onClick={myfunction}>How do i cancel?</div>
+        <div className="hi">Netflix is flexible. There are no annoying contracts and no
+         commitments. You can easily cancel your account online in two clicks. 
+        There are no cancellation fees – start or stop your account anytime.</div>
+
+        <div  className="divstyle" onClick={myfunction}>What can i watch on Netflix?</div>
+        <div className="hi">Netflix has an extensive library of feature films, documentaries, 
+        TV shows, anime, award-winning Netflix originals, and more.
+         Watch as much as you want, anytime you want.</div>
     </div>
 )
 
