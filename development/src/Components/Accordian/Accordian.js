3353a59e1a496d3c2f66941c84703e76629c7989
shivank goyal <goyal.shivank790@gmail.com>
2021-03-01 19:10:29 +0530

    accordianfix

--- development/src/Components/Accordian/Accordian.js
+++ development/src/Components/Accordian/Accordian.js
@@ -1,20 +1,70 @@
 import React  from 'react'
 import "./Accordian.css"
 
-const myfunction = ()  =>{
-    
-    var z=document.getElementsByClassName('divstyle');
-    var i;
-    for(i=0;i < z.length; i++){
 
-        z[i].classList.toggle("active");
-        var y=z[i].nextElementSibling;
-        if(y.style.display === "none")
-            y.style.display = "block";
-        else
-            y.style.display ="none";
+const myfunction1 = () => {
+    var z=document.getElementsByClassName("divstyle");
+    var y=document.getElementsByClassName("hi")
+    z[0].classList.toggle("active");
+    if(y[0].style.display === "none"){
+        y[0].style.display ="block";
+      }
+    else{
+        y[0].style.display ="none";
+  
+       }
 
-    }
+}
+
+const myfunction2 = () => {
+    var z=document.getElementsByClassName("divstyle");
+    var y=document.getElementsByClassName("hi");
+
+    z[1].classList.toggle("active");
+    if(y[1].style.display === "none"){
+        y[1].style.display ="block";
+        }
+    else
+        y[1].style.display ="none";    
+
+}
+
+const myfunction3 = () => {
+    var z=document.getElementsByClassName("divstyle");
+    var y=document.getElementsByClassName("hi")
+
+    z[2].classList.toggle("active");
+    if(y[2].style.display === "none"){
+        y[2].style.display ="block";
+        }
+    else
+        y[2].style.display ="none";    
+
+}
+
+const myfunction4 = () => {
+    var z=document.getElementsByClassName("divstyle");
+    var y=document.getElementsByClassName("hi")
+
+    z[3].classList.toggle("active");
+    if(y[3].style.display === "none"){
+        y[3].style.display ="block";
+        }
+    else
+        y[3].style.display ="none";    
+
+}
+const myfunction5 = () => {
+    var z=document.getElementsByClassName("divstyle");
+    var y=document.getElementsByClassName("hi")
+
+    z[4].classList.toggle("active");
+    if(y[4].style.display === "none"){
+        
+   
+        y[4].style.display ="block";}
+    else
+        y[4].style.display ="none";    
 
 }
  
@@ -25,7 +75,7 @@ const myfunction = ()  =>{
 const Accord= () =>{
 return(
     <div>
-        <div  className="divstyle" onClick={myfunction}>
+        <div  className="divstyle" onClick={myfunction1}>
             What is Netflix?
         </div>
         <div className="hi">
@@ -38,11 +88,11 @@ return(
           always something new to discover, and new TV shows and 
           movies are added every week!
         </div>
-        <div  className="divstyle" onClick={myfunction}>How much does Netflix cost?</div>
+        <div  className="divstyle" onClick={myfunction2}>How much does Netflix cost?</div>
         <div className="hi">
             aksndljbasdasdsa jfbasd
         </div>
-        <div  className="divstyle" onClick={myfunction}>Where can i watch?</div>
+        <div  className="divstyle" onClick={myfunction3}>Where can i watch?</div>
         <div className="hi"><p>Watch anywhere, anytime, on an unlimited number of devices. 
         Sign in with your Netflix account to watch instantly on the web at 
         netflix.com from your personal computer or on any internet-
@@ -56,12 +106,12 @@ return(
           </p>
           </div>
 
-        <div  className="divstyle" onClick={myfunction}>How do i cancel?</div>
+        <div  className="divstyle" onClick={myfunction4}>How do i cancel?</div>
         <div className="hi">Netflix is flexible. There are no annoying contracts and no
          commitments. You can easily cancel your account online in two clicks. 
         There are no cancellation fees – start or stop your account anytime.</div>
 
-        <div  className="divstyle" onClick={myfunction}>What can i watch on Netflix?</div>
+        <div  className="divstyle" onClick={myfunction5}>What can i watch on Netflix?</div>
         <div className="hi">Netflix has an extensive library of feature films, documentaries, 
         TV shows, anime, award-winning Netflix originals, and more.
          Watch as much as you want, anytime you want.</div>
