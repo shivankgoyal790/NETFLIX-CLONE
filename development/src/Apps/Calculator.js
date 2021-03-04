4c4fff16b2025fa50157decb77d5b40e831759ca
shivank goyal <goyal.shivank790@gmail.com>
2021-03-04 23:08:00 +0530

    sqrt divide and cross

--- development/src/Apps/Calculator.js
+++ development/src/Apps/Calculator.js
@@ -4,6 +4,8 @@ import logo from "G:/react_practice/development/src/Components/images/images.png
 var y;
 var z;
 var x;
+var b;
+var c;
 
 class Calculator extends React.Component {
     
@@ -14,28 +16,68 @@ class Calculator extends React.Component {
             document.getElementById("inpt").value = "";
             document.getElementById("inpt").placeholder = 0;
         }
-        else if(a !== "+" && a !== "=" && a !== "-"){
+
+        else if(a === "x"){
+            var stn = document.getElementById("inpt").value ;
+            document.getElementById("inpt").value = stn.substring(0,stn.length-1);
+            y=  document.getElementById("inpt").value;
+        }
+
+        else if (a === "sq")
+        {
+            document.getElementById("inpt").value = Math.sqrt(Number(y));
+            y =  document.getElementById("inpt").value ;
+        }
+
+        else if(a !== "+" && a !== "=" && a !== "-" && a !== "*" && a !== "/" && a !== "1/x" && a !== "sq"){
         document.getElementById("inpt").value += a;
         y =  document.getElementById("inpt").value;
         }
-        else if(a === "+" || a === "-")
+
+        else if(a === "+" || a === "-" || a === "*" || a === "/" )
         {
             x = a;
             document.getElementById("inpt").value = "";
             document.getElementById("inpt").placeholder = y;
             z = y;
-            y= "";
+            y = "";
             
         }
 
+        else if(a === "1/x")
+        {
+            document.getElementById("inpt").value = 1 / Number(y);
+            y=  document.getElementById("inpt").value;
+        }
+
+
 
         else if(a === "="){
-            if(x === "+")
-            document.getElementById("inpt").value = Number(y)+ Number(z);
+            if(x === "+"){
+                document.getElementById("inpt").value = Number(y) + Number(z);
+                b=  Number(y)+ Number(z);
+                y = b.toString();
+            }
             if(x === "-")
-            document.getElementById("inpt").value = Number(z)- Number(y);
-            y="";
-            z="";
+            {
+                document.getElementById("inpt").value = Number(z) - Number(y);
+                c=Number(z)- Number(y)
+                y = c.toString();
+            }
+
+            if(x === "/")
+            {
+                document.getElementById("inpt").value = Number(z) / Number(y);
+                c=Number(z)/Number(y);
+                y = c.toString();
+            }
+
+            if(x === "*")
+            {
+                document.getElementById("inpt").value = Number(z)*Number(y);
+                c=Number(z)*Number(y)
+                y = c.toString();
+            }
 
         }
 
@@ -56,19 +98,19 @@ class Calculator extends React.Component {
                         <div className="btn">M</div>&emsp;
                         <div className="btn" onClick = {() => this.shoot("CE")}>CE</div>
                         <div className="btn" onClick = {() => this.shoot("C")}>C</div>
-                        <div className="btn">X</div>
-                        <div className="btn"> &#xf7;</div>
+                        <div className="btn"  onClick = {() => this.shoot("x")}>&#9876;</div>
+                        <div className="btn" onClick = {() => this.shoot("/")}> &#xf7;</div>
                         
                         
                         
                     </div>
                     
                     <div className="flex-container">
-                        <div className="btn">M</div>&emsp;
+                        <div className="btn"> &#xb1; </div>&emsp;
                         <div className="btn" onClick = {() => this.shoot("7")} >7 </div>
                         <div className="btn" onClick = {() => this.shoot("8")}>8</div>
                         <div className="btn" onClick = {() => this.shoot("9")}>9</div>
-                        <div className="btn">x</div>
+                        <div className="btn"  onClick = {() => this.shoot("*")}>*</div>
                         <br/>
                         
                         
@@ -76,7 +118,7 @@ class Calculator extends React.Component {
 
                          
                     <div className="flex-container">
-                        <div className="btn">&radic;</div>&emsp;
+                        <div className="btn" onClick = {() => this.shoot("sq")}>&radic;</div>&emsp;
                         <div className="btn" onClick = {() => this.shoot("4")}>4</div>
                         <div className="btn" onClick = {() => this.shoot("5")}>5</div>
                         <div className="btn" onClick = {() => this.shoot("6")}>6</div>
@@ -99,7 +141,7 @@ class Calculator extends React.Component {
                     </div>
 
                     <div className="flex-container">
-                        <div className="btn">1/x</div>&emsp;
+                        <div className="btn" onClick = {() => this.shoot("1/x")}>1/x</div>&emsp;
                         <div className="btn" onClick = {() => this.shoot("0")}>0</div>
                         <div className="btn" onClick = {() => this.shoot("00")}>00</div>
                         <div className="btn" onClick = {() => this.shoot(".")}>.</div>
